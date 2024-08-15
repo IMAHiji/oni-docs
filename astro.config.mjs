b35a4cb7b9d8e9a85aaf1791685254a778b35eee
Luke <lukeaskins@gmail.com>
2024-08-15 09:04:34 -0500

    Adds forwd[D[Dslash to base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://IMAHiji.github.io',
-    base: 'oni-docs',
+    base: '/oni-docs',
 });
